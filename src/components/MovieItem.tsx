fc8fc1f8f647b4c46b638ec5120eb2f30f994763
Camila Ledezma <camilaledezmar@gmail.com>
2024-09-03 12:22:05 -0400

    home details

--- src/components/MovieItem.tsx
+++ src/components/MovieItem.tsx
@@ -1,19 +1,20 @@
 import { Movie } from '../models/Movie';
-import {useNavigate, Link} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
 type MovieItemProps = {
   movie: Movie;
 };
 
 const MovieItem = ({ movie }: MovieItemProps) => {
-const navigate = useNavigate();
+// const navigate = useNavigate();
   return (
-    <div className="movie-item-container" onClick={() => {navigate(`/movies/${movie.id}`)}}>
+    // <div className="movie-item-container" onClick={() => {navigate(`/MovieApp/movies/${movie.id}`)}}>
+    <div>  
       <img src={movie.poster_path} alt={`${movie.title} Poster`} className="movie-item-poster" />
       <div className="movie-item-details">
         <h2 className="movie-item-title">{movie.title}</h2>
         <p className="movie-item-language">Sub|Dob - {movie.original_language}</p>
-        <Link to = {`/movies/${movie.id}`}>Go to movie details </Link>
+        <Link to = {`/MovieApp/movies/${movie.id}`}>Go to movie details </Link>
       </div>
     </div>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,16 @@
-const Home = () =>{
-return(
-  <div>
-    <h1>Soy home xD</h1>
-  </div>
-)
-}
+import { useNavigate } from 'react-router-dom';
+
+const Home = () => {
+  const navigate = useNavigate();
+  
+  return (
+    <div>
+      <h1>Welcome to MovieApp!</h1>
+      <div className ='goMovies' onClick={() => { navigate(`/MovieApp/movies`) }}>
+        Click here to go to movies
+      </div>
+    </div>
+  );
+};
+
 export default Home;
